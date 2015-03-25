804517ba94cba4ca5bbc937a0caf1d7e6faac1d2
LawrenceHan <yangfei6565@163.com>
2015-03-25 23:07:32 +0800

    Created SpriteKit test project

---
+++ HelloSpriteKit/HelloSpriteKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloSpriteKit
+//
+//  Created by Hanguang on 3/25/15.
+//  Copyright (c) 2015 Hanguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
