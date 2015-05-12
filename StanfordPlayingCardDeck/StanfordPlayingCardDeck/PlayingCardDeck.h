7986fd31e1d4fc30113312b007c2346345b523bc
LawrenceHan <yangfei6565@163.com>
2015-05-12 11:59:41 +0800

    Added new models.

---
+++ StanfordPlayingCardDeck/StanfordPlayingCardDeck/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  StanfordPlayingCardDeck
+//
+//  Created by Hanguang on 5/12/15.
+//  Copyright (c) 2015 Hanguang. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
