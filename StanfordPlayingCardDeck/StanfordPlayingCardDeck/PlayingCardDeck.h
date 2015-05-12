ec5fb67b6ca58c0d2d25c180817431e25542fe38
LawrenceHan <yangfei6565@163.com>
2015-05-12 15:46:15 +0800

    Finished home work. Close #17

--- StanfordPlayingCardDeck/StanfordPlayingCardDeck/PlayingCardDeck.h
+++ StanfordPlayingCardDeck/StanfordPlayingCardDeck/PlayingCardDeck.h
@@ -10,4 +10,5 @@
 
 @interface PlayingCardDeck : Deck
 
+
 @end
--- StanfordPlayingCardDeck/StanfordPlayingCardDeck/ViewController.m
+++ StanfordPlayingCardDeck/StanfordPlayingCardDeck/ViewController.m
@@ -7,9 +7,15 @@
 //
 
 #import "ViewController.h"
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipsCount;
+@property (strong, nonatomic) PlayingCardDeck *playingDeck;
+@property (strong, nonatomic) AVAudioPlayer *audioPlayer;
 @end
 
 @implementation ViewController
@@ -19,9 +25,39 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (PlayingCardDeck *)playingDeck {
+    if (!_playingDeck) _playingDeck = [[PlayingCardDeck alloc] init];
+    return _playingDeck;
 }
+
+- (AVAudioPlayer *)audioPlayer {
+    if (!_audioPlayer) {
+        NSURL *fileURL = [[NSBundle mainBundle] URLForResource:@"flip" withExtension:@"wav"];
+        _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL error:nil];
+    }
+    return _audioPlayer;
+}
+
+- (void)setFlipsCount:(int)flipsCount {
+    _flipsCount = flipsCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipsCount];
+}
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardBack"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+        
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardFront"]
+                          forState:UIControlStateNormal];
+        Card *randomCard = [self.playingDeck drawRandomCard];
+        [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+        [self.audioPlayer play];
+    }
+    self.flipsCount++;
+}
+
+
 
 @end
