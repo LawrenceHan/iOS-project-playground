d8fe71bf690385259032de703349c4c3988a4b21
LawrenceHan <yangfei6565@163.com>
2015-08-20 15:34:38 +0800

    add test project

---
+++ 12_TouchTracker/TouchTracker/BNRLine.h
@@ -0,0 +1,17 @@
+//
+//  BNRLine.h
+//  TouchTracker
+//
+//  Created by John Gallagher on 1/9/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRLine : UIView
+
+@property (nonatomic) CGPoint begin;
+@property (nonatomic) CGPoint end;
+@property (nonatomic) CGFloat lineWidth;
+
+@end
