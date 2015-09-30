fec46b31aa1bd630480a543feaec0d5a06ffff28
LawrenceHan <yangfei6565@163.com>
2015-09-30 12:11:55 +0800

    Bump version

--- RaiseMan/RaiseMan/Employee.swift
+++ RaiseMan/RaiseMan/Employee.swift
@@ -11,5 +11,16 @@ import Foundation
 class Employee: NSObject {
     var name: String? = "New Employee"
     var raise: Float = 0.05
+    
+    // MARK: Validatation delegate
+    func validateRaise(raiseNumberPointer: AutoreleasingUnsafeMutablePointer<NSNumber?>) throws {
+            let raiseNumber = raiseNumberPointer.memory
+            if raiseNumber == nil {
+                let domain = "UserInputValidationErrorDomain"
+                let code = 0
+                let userInfo = [NSLocalizedDescriptionKey : "An employee's raise must be a number."]
+                throw NSError(domain: domain, code: code, userInfo: userInfo)
+            }
+    }
 }
 
