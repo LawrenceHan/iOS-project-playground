74b2c0f1f2ebde57858a41fdb74fe402fb1d4763
LawrenceHan <yangfei6565@163.com>
2016-01-14 17:20:18 +0800

    Added new file.

--- OAuth2Test/OAuth2Test/MainWindowController.swift
+++ OAuth2Test/OAuth2Test/MainWindowController.swift
@@ -10,6 +10,10 @@ import Cocoa
 
 class MainWindowController: NSWindowController {
 
+    override var windowNibName: String {
+        return "MainWindowController"
+    }
+    
     override func windowDidLoad() {
         super.windowDidLoad()
 
