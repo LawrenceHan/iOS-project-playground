2efadbe8a2807dd954d0196c61a738e8b72a559a
LawrenceHan <yangfei6565@163.com>
2016-01-14 17:19:57 +0800

    Added new test project.

---
+++ OAuth2Test/OAuth2Test/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  OAuth2Test
+//
+//  Created by Hanguang on 1/14/16.
+//  Copyright © 2016 Hanguang. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
